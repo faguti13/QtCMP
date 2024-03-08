bdc289fc13b65173783f486b3c794fb87f951bc7
AsBarahona <asbarahonam@gmail.com>
2024-03-07 23:55:09 -0600

    Creacion de los nodos de la lista enlazada doble

--- principallist.cpp
+++ principallist.cpp
@@ -16,6 +16,67 @@ namespace pt = boost::property_tree;
 using namespace std;
 using namespace TagLib;
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct Node { //Estructura del nodo (def el contenido de cada uno)
+    std::string title;
+    std::string artist;
+    std::string album;
+    std::string genre;
+    std::string reference;
+    int upVotes;
+    int downVotes;
+
+    Node* prev;
+    Node* next;
+
+    Node(const std::string& t, const std::string& a, const std::string& al, const std::string& g,const std::string& r, int uV, int dV)
+        : title(t), artist(a), album(al), genre(g), reference(r), upVotes(uV), downVotes(dV), prev(nullptr), next(nullptr) {}
+};
+
+// Clase lista doblemente enlazada
+class DoublyLinkedList {
+private:
+    Node* head; //Punteros
+    Node* tail;
+
+public:
+    DoublyLinkedList() : head(nullptr), tail(nullptr) {}
+
+    // Método para insertar en la lista
+    void insertEnd(const std::string& title, const std::string& artist, const std::string& album, const std::string& genre,
+                         const std::string& reference, int upVotes, int downVotes) {
+        Node* newNode = new Node(title, artist, album, genre,  reference, upVotes, downVotes);
+        if (head == nullptr) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+    }
+
+    // Método para imprimir toda la info de los nodos (para verificar)
+    void printForward() {
+        Node* current = head;
+        while (current != nullptr) {
+            std::cout << "Canción: " << current->title << " , " << current->artist << " , " << current->album
+                      << " , " << current->genre<< " , " << current->reference
+                      << " , " << current->upVotes << " , " << current->downVotes << std::endl;
+            current = current->next;
+        }
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+// Clase para la lista principal (inicio)
+class principalList {
+public:
+    principalList(); // Declaración del constructor
+};
+
 principalLIst::principalLIst() { // Constructor de principalLIst
     //Abre el .ini para obtener la ruta de la carpeta
     try {
@@ -34,9 +95,11 @@ principalLIst::principalLIst() { // Constructor de principalLIst
     }
 }
 
+DoublyLinkedList principalLinkedList;
+
 void listFilesInFolder(const std::string& folderPath) { // Método para saber el path de cada .mp3
     for (const auto& entry : fs::directory_iterator(folderPath)) {
-        std::cout << entry.path() << std::endl;
+        //std::cout << entry.path() << std::endl; //Comprobar que pase bien el path
         std::string filePathArchive = entry.path();
         loadAndPrintMetadata(filePathArchive); // Llama al método que carga y muestra los metadatos
     }
@@ -50,16 +113,23 @@ void loadAndPrintMetadata(const std::string& filePath) { // Método para cargar 
     //(ID3 es un contenedor de metadatos)
     Tag *tag = file.ID3v2Tag();
 
-    // Imprimir los metadatos en pantalla
+    // Acceder a los tags
     if (tag) {
-        cout << "Título: " << tag->title().toCString(true) << endl;
-        cout << "Artista: " << tag->artist().toCString(true) << endl;
-        cout << "Álbum: " << tag->album().toCString(true) << endl;
-        cout << "Año: " << tag->year() << endl;
-        cout << "Género: " << tag->genre().toCString(true) << endl;
+        //cout << "Título: " << tag->title().toCString(true) << endl; // Prubas para verificar que obtenga los metadatos bien
+        //cout << "Artista: " << tag->artist().toCString(true) << endl;
+        //cout << "Álbum: " << tag->album().toCString(true) << endl;
+        //cout << "Género: " << tag->genre().toCString(true) << endl;
+        //cout << "Referencia: " << filePath << endl;
+
+        // Inserta los metadatos en un nodo de la lista
+        principalLinkedList.insertEnd(tag->title().toCString(true), tag->artist().toCString(true), tag->album().toCString(true),
+                                      tag->genre().toCString(true), filePath, 0, 0);
+
+        std::cout << "Nodos actuales en la lista:" << std::endl;
+        principalLinkedList.printForward();
 
     } else {
         cout << "No se encontraron metadatos en el archivo." << endl;
-    }
-}
+    }}
+
 
